f19fa3697f18c44c73bde66a2d74951301a1fd69
YesminArias <yyariase@gmail.com>
2022-07-28 10:49:47 -0500

    delete header

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ server.use(cors())
 server.name = 'API'
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
