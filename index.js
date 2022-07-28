d69d8d1a28b7ff42605dddca54977cfd73d191a2
YesminArias <yyariase@gmail.com>
2022-07-27 23:06:56 -0500

    heroku

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const port = process.env.PORT || 3001;
 const cors = require('cors')
-const app = express();
+
 const server = require('./src/app.js')
 
-app.use(cors())
+
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
